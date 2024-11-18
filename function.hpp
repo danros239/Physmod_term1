ff9ed848b50cff2f8a921496c37808318e5fae0c
sk <sergeykorovichev@gmail.com>
2024-11-18 12:39:17 +0300

    Added Euler, Heun, Runge-Kutta(4) and visualisations in jupyter

--- function.hpp
+++ function.hpp
@@ -35,6 +35,13 @@ class SingleState // : public State<T>
         return res;
     }
 
+    SingleState ts(T dt) // returns state shifted in time by dt
+    {
+        SingleState other = *this;
+        other.t += dt;
+        return other;
+    }
+
     void print()
     {
         std::cout << x << " " << v << std::endl;
@@ -45,13 +52,28 @@ template <typename T, template <typename F_T> class Function>
 class Abstract_Solver
 {
     public:
+    void euler_solve(SingleState<T>& state, Function<T> foo, T dt)
+    {
+        SingleState<T> delta = foo(state)*dt;
+        state = state + delta;
+        state.t += dt;
+    }
+
+    void heun_solve(SingleState<T>& state, Function<T> foo, T dt)
+    {
+        SingleState<T> k1 = foo(state)*dt*(T)0.5;
+        SingleState<T> k2 = foo(state.ts(dt*(T)0.5) + k1);
+        state = state + k2*(T)dt;
+        state.t += dt;
+    }
+
     void rk_solve(SingleState<T>& state, Function<T> foo, T dt)
     {
         SingleState<T> k1(0,0,state.t), k2(0,0,state.t), k3(0,0,state.t), k4(0,0,state.t);
         k1 = foo(state);
-        k2 = foo(state + k1*dt*static_cast<T>(0.5));
-        k3 = foo(state + k2*dt*static_cast<T>(0.5));
-        k4 = foo(state + k3*dt);
+        k2 = foo(state.ts(dt*(T)0.5) + k1*dt*static_cast<T>(0.5));
+        k3 = foo(state.ts(dt*(T)0.5) + k2*dt*static_cast<T>(0.5));
+        k4 = foo(state.ts(dt) + k3*dt);
         state = state + (k1*(T)(1/(T)6) + k2*(T)(1/(T)3) + k3*(T)(1/(T)3) + k4*(T)(1/(T)6))*dt;
         state.t += dt;
     }
--- task1.cpp
+++ task1.cpp
@@ -8,15 +8,25 @@ int main(int argc, char* argv[])
 {
     std::ifstream file(argv[1]);
     json data = json::parse(file);
+    file.close();
+
     std::cout << data.dump() << std::endl;
+    std::string algorithm = data["algorithm"];
     
     Abstract_Solver<float, MPendulum> as;
 
     MPendulum<float> force(1.0f);
     SingleState<float> basic_pendulum(data["x0"], data["y0"]);
 
-    as.rk_solve(basic_pendulum, force, data["step"]);
+    std::ofstream output("euler.csv");
+    output << "t x v" << std::endl;
+    for(int i=0; i<1000; i++)
+    {
+        as.euler_solve(basic_pendulum, force, data["step"]);
+        basic_pendulum.print();
+        output << " " << basic_pendulum.t << " " << basic_pendulum.x << " " << basic_pendulum.v << std::endl;
+    }
+    output.close();
 
-    basic_pendulum.print();
-    file.close();
+
 }
